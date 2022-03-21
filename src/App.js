39d4f69ad8cb369459b65739bad3f20a73fe7040
Jenna Hakulinen <jennash@k-d557-mac-32.pk.amk.root>
2022-03-21 10:55:04 +0200

    ajax routing valmis

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 // import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-import MediaTable from './components/MediaTable';
+import Nav from './components/Nav';
+import Home from './views/Home';
+import Profile from './views/Profile';
+import Single from './views/Single';
 
 const App = () => {
-  return <MediaTable />;
+  return (
+    // eslint-disable-next-line no-undef
+    <Router basename={process.env.PUBLIC_URL}>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/single" element={<Single />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,11 @@
+import MediaTable from '../components/MediaTable';
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <MediaTable />
+    </>
+  );
+};
+
+export default Home;
