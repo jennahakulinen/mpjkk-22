938b039e7825cf43928c60eb30de266c6316f4c3
Jenna Hakulinen <jennash@k-d557-mac-32.pk.amk.root>
2022-03-25 13:45:35 +0200

    login valmis

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,37 @@
 // eslint-disable-next-line no-unused-vars
 import PropTypes from 'prop-types';
+import useForm from '../hooks/FormHooks';
 
 const LoginForm = (props) => {
-  return <div>LoginForm</div>;
+  const alkuarvot = {
+    username: '',
+    password: '',
+  };
+
+  const doLogin = () => {
+    console.log('doLogin');
+  };
+  // TODO: add login functionalities here
+  const {inputs, handleInputChange} = useForm(doLogin, alkuarvot);
+  console.log(inputs);
+  return (
+    <form>
+      <input
+        placeholder="username"
+        name="username"
+        onChange={handleInputChange}
+        value={inputs.username}
+      />
+      <input
+        placeholder="password"
+        name="password"
+        type="password"
+        onChange={handleInputChange}
+        value={inputs.password}
+      />
+      <input type="submit" value="Login" />
+    </form>
+  );
 };
 
 LoginForm.propTypes = {};
