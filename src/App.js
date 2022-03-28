b1852fc4afa5a36ac1fcca786c09da301ec92c4a
Jenna Hakulinen <jennash@k-d557-mac-32.pk.amk.root>
2022-03-28 10:39:11 +0300

    login 12.

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ import Login from './views/Login';
 import Profile from './views/Profile';
 import Single from './views/Single';
 import Logout from './views/Logout';
+import {MediaProvider} from './contexts/MediaContext';
 
 const App = () => {
   return (
     // eslint-disable-next-line no-undef
     <Router basename={process.env.PUBLIC_URL}>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/single" element={<Single />} />
-        <Route path="/logout" element={<Logout />} />
-      </Routes>
+      <MediaProvider>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/single" element={<Single />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </MediaProvider>
     </Router>
   );
 };
--- src/views/Logout.js
+++ src/views/Logout.js
@@ -1,8 +1,12 @@
+import {useContext} from 'react';
 import {Navigate} from 'react-router-dom';
+import {MediaContext} from '../contexts/MediaContext';
 
 const Logout = () => {
+  const [user, setUser] = useContext(MediaContext);
+  setUser(null);
   localStorage.clear();
-  return <Navigate to="/" />;
+  return <>{!user ? <Navigate to="/" /> : <div>Loading...</div>}</>;
 };
 
 export default Logout;
