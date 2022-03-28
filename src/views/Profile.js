b8bb9debb68f9e19bf29e04156df75f8c177002c
Jenna Hakulinen <jennash@k-d557-mac-32.pk.amk.root>
2022-03-28 12:05:39 +0300

    material-ui b

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,26 +1,36 @@
-import {useUser} from '../hooks/ApiHooks';
-import {useState, useEffect} from 'react';
+import {useContext, useState, useEffect} from 'react';
+import {MediaContext} from '../contexts/MediaContext';
+import {useTag} from '../hooks/ApiHooks';
+import {mediaUrl} from '../utils/variables';
 
 const Profile = () => {
-  const [user, setUser] = useState({});
-  const {getUser} = useUser();
-  const fetchUser = async () => {
-    const userData = await getUser(localStorage.getItem('token'));
-    setUser(userData);
-  };
+  const [user] = useContext(MediaContext);
+  const [avatar, setAvatar] = useState({
+    filename: 'https://placekitten.com/320',
+  });
+  const {getTag} = useTag();
 
+  const fetchAvatar = async () => {
+    const avatars = await getTag('avatar_' + user.user_id);
+    setAvatar(avatars.pop());
+  };
   useEffect(() => {
-    fetchUser();
-  }, []);
-  console.log(user);
+    fetchAvatar(user);
+  }, [user]);
+
   return (
     <>
       <h1>Profile</h1>
-      <ul>
-        <li>{user.username}</li>
-        <li>{user.email}</li>
-        <li>{user.full_name}</li>
-      </ul>
+      {user && (
+        <ul>
+          <li>
+            <img src={mediaUrl + avatar.filename} alt={user.username} />
+          </li>
+          <li>{user.username}</li>
+          <li>{user.email}</li>
+          <li>{user.full_name}</li>
+        </ul>
+      )}
     </>
   );
 };
